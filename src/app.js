72fb011d0ceaf4198ed49168fb9d286c4d61d9cb
plotier <plotier15@gmail.com>
2021-05-21 18:12:54 +0000

    repo excuse gen

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  //let randomNumber = Math.floor(Math.random() * 10);
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  //console.log(randomNumber);
+};
+let generateExcuse = () => {
+  let pronoun = ["el", "un"];
+  let subject = [
+    "policia",
+    "ladron",
+    "cocinero",
+    "politico",
+    "Death Metal",
+    "Age of Empires"
+  ];
+  let action = ["robó mi", "impidió mi", "retrasó mi", "me sacó la"];
+  let possetion = ["voluntad", "tarea", "sinceridad", "ganas de vivir"];
+  let where = ["en la calle", "en el barrio", "acá cerca", "en la vinoteca"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
